644e314be3243f6f2517c8e4aab2ec9ca6c49c95
jeonghun <vvv4106@naver.com>
2018-10-18 11:21:52 +0900

    This is the first commit.

---
+++ SmartFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SmartFactory
+{
+    class SmartFactory
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            Console.WriteLine("{0} dddd", i);
+        }
+
+        
+    }
+}
